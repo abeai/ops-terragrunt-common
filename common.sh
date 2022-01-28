a0e229640d54a75d3b166ab6c7bb488f407aae0e
Kody Peterson <36930286+kody-abe@users.noreply.github.com>
2022-01-28 16:11:20 -0500

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,3 +1,5 @@
 curl -s https://raw.githubusercontent.com/abeai/ops-terragrunt-common/main/common.hcl > .terragrunt/common.hcl
 curl -s https://raw.githubusercontent.com/abeai/ops-terragrunt-common/main/getRancherAPIAdminToken.sh > .terragrunt/getRancherAPIAdminToken.sh
 curl -s https://raw.githubusercontent.com/abeai/ops-terragrunt-common/main/getRancherAPIUrl.sh > .terragrunt/getRancherAPIUrl.sh
+
+echo ".terragrunt/common.hcl"
